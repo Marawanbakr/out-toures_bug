83365050643cd7a503cb5a9ffbca11f9062c8c61
Marwanbakr <mrawanbakr7@gmail.com>
2021-11-23 04:34:14 +0200

    resolve my bug

---
+++ our-tours/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tours from './tour'
+function App() {
+  return (
+    <div className='myapp'>
+      <h1> Our Tours</h1>
+      <Tours />
+    </div>
+  )
+}
+
+export default App
---
+++ our-tours/src/tour.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { data } from './ourdata'
+
+const Tours = () => {
+  const [oursData, setOurData] = useState(data)
+
+  const removeItem = (id) => {
+    let card = oursData.filter((item) => item.id !== id)
+    setOurData(card)
+  }
+
+  const ReadMore = ({ children }) => {
+    const text = children
+    console.log
+    const [isReadMore, setIsReadMore] = useState(true)
+    const toggleReadMore = () => {
+      setIsReadMore(!isReadMore)
+    }
+    return (
+      <p className='text'>
+        {isReadMore ? text.slice(0, 210) : text}
+        <span onClick={toggleReadMore} className='read-or-hide'>
+          {isReadMore ? '...read more' : ' show less'}
+        </span>
+      </p>
+    )
+  }
+  return (
+    <React.Fragment>
+      {oursData.map((card) => {
+        const { id, img, title, heading, price, button } = card
+        return (
+          <div key={id} className='card'>
+            <div className='image'>
+              <img src={img}></img>
+            </div>
+            <div className='information'>
+              <div className='box'>
+                <div className='pragraph'>{heading}</div>
+                <div className='price'>{price}</div>
+              </div>
+              <ReadMore>{title}</ReadMore>
+              <button onClick={() => removeItem(id)}>{button}</button>
+            </div>
+          </div>
+        )
+      })}
+    </React.Fragment>
+  )
+}
+export default Tours
