2eaf6323f335eb060d96d54a06f3d987abc49e8f
Ahmed Younes <cahmedc@gmail.com>
2021-11-24 14:58:02 +0200

    fix

--- our-tours/src/App.js
+++ our-tours/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Tours from './tour'
+import React from 'react';
+// import Tours from './tour
+import Tours from './tourv2';
 function App() {
   return (
-    <div className='myapp'>
+    <div className="myapp">
       <h1> Our Tours</h1>
       <Tours />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
